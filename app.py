e7589599ec6ebf6a429cb93c97b7e89a5d8c4d0a
unknown <zammaromar1@gmail.com>
2025-02-23 00:02:37 +0200

    creating route for prediction in flask

--- app.py
+++ app.py
@@ -9,3 +9,23 @@ text = "I absolutely love this product! It's amazing."
 scores = analyzer.polarity_scores(text)
 print(scores)
 
+app = Flask(__name__)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        data = request.get_json()
+        text = data.get("text", "Guest")
+        prediction = analyzer.polarity_scores(text)
+        if(prediction['neg'] >= 0.5):
+            # return jsonify({"prediction": f"{prediction}"})
+            return jsonify({'prediction': 'negative'})
+        elif(prediction['pos'] >= 0.5):
+            return jsonify({'prediction': 'positive'})
+        else:
+            return jsonify({'prediction': 'neutral'})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+    
+if __name__ == "__main__":
+    app.run(debug=True)
