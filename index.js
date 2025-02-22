348f14187b2c60df8a79a3a1a814897f732568d5
unknown <zammaromar1@gmail.com>
2025-02-23 00:04:56 +0200

    creating express app to communicate with flask server using axios

---
+++ index.js
@@ -0,0 +1,17 @@
+const axios = require("axios")
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+app.use(bodyParser.json())
+
+app.post("/ml-predict", (req, res) => {
+    console.log("calling ML api ...")
+    const itext = req.body.text
+    axios.post("http://127.0.0.1:5000/predict", {
+        "text": itext
+    }).then((mlRes) => {
+        console.log("ML api response: ", mlRes.data)
+        res.send(mlRes.data)
+    }).catch(error => console.error(error))
+})
